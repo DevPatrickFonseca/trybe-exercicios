7d66ea0e51553eeffe1bf817a855a3df937c1296
pfonsecafv <pfonsecafv@gmail.com>
2022-10-21 19:18:17 -0300

    Exercício 04 - Imprimir mensagem se a  Média dos valores da array é maior que 20

---
+++ m1-fundamentos/secao-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/exercicio-04.js
@@ -0,0 +1,15 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let soma = 0;
+
+for (let index = 0; index < numbers.length; index += 1) {
+  soma += numbers[index];
+}
+
+soma = soma / numbers.length;
+
+if (soma > 20) {
+  console.log("O valor é maior que 20");
+} else {
+  console.log("O valor é menor ou igual a 20");
+}
