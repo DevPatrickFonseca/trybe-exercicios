4cb01f0f36382d4c067c75977317e507b90be682
pfonsecafv <pfonsecafv@gmail.com>
2022-10-26 18:29:16 -0300

    Exercício 05 :rocket: - Função nome para arrow function

---
+++ m1-fundamentos/secao-04-introducao-a-javascript/dia-05-javascript-es6-let-const-arrow-functions-e-template-literals/exercise-rocket-05.js
@@ -0,0 +1,15 @@
+// function nomeCompleto(nome, sobrenome) {
+//   return `${nome} ${sobrenome}`
+// }
+// let nome = 'Ivan';
+// let sobrenome = 'Pires';
+// console.log(nomeCompleto(nome, sobrenome));
+
+// Forma arrow function
+
+const nomeCompleto = (nome, sobrenome) => `${nome} ${sobrenome}`
+
+let nome = 'Ivan';
+let sobrenome = 'Pires';
+
+console.log(nomeCompleto(nome, sobrenome));
