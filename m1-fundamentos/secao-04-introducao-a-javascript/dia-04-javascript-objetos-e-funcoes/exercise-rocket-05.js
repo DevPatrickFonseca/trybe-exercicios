0394ce18f093668440071d7f76907bfe78c3ae87
pfonsecafv <pfonsecafv@gmail.com>
2022-10-25 22:01:43 -0300

    Exercício 05 :rocket: - Listar chaves

---
+++ m1-fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/exercise-rocket-05.js
@@ -0,0 +1,14 @@
+let leitor = {
+  nome: 'Julia',
+  sobrenome: 'Pessoa',
+  idade: 21,
+  livrosFavoritos: [
+    {
+      titulo: 'O Pior Dia de Todos',
+      autor: 'Daniela Kopsch',
+      editora: 'Tordesilhas',
+    },
+  ],
+}
+
+console.log(leitor.nome + " " + leitor.sobrenome + " " + leitor.livrosFavoritos[0].titulo);
