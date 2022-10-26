d4b46dc282ad34fa003278e8b304f19301a1d213
pfonsecafv <pfonsecafv@gmail.com>
2022-10-26 16:49:25 -0300

    Exercício 08 - Template literals

---
+++ m1-fundamentos/secao-04-introducao-a-javascript/dia-05-javascript-es6-let-const-arrow-functions-e-template-literals/exercise-08.js
@@ -0,0 +1,17 @@
+// Caso 01
+const myName = 'Isabella';
+console.log('Hello' + ' ' + myName + '!');
+
+// Caso 02
+const myName2 = 'Isabella';
+console.log(`Welcome ${myName2}!`);
+
+// Caso 03
+// Com o template literals
+console.log(`Primeira linha;
+Segunda linha;
+Terceira linha;`
+);
+
+// Sem o template literals
+console.log('Primeira linha;\n' + 'Segunda linha;\n' + 'Terceira linha;\n');
