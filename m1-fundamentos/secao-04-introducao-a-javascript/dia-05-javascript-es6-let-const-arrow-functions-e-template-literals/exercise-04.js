327d328378237cadbd2cb6ca4225ddb17676a965
pfonsecafv <pfonsecafv@gmail.com>
2022-10-26 16:43:06 -0300

    Exercício 04 - Let e Const

---
+++ m1-fundamentos/secao-04-introducao-a-javascript/dia-05-javascript-es6-let-const-arrow-functions-e-template-literals/exercise-04.js
@@ -0,0 +1,9 @@
+const favoriteLanguage = 'Javascript';
+favoriteLanguage = 'Python';
+console.log(favoriteLanguage); 
+// Vai dar erro
+
+let favoriteTechnology = 'Machine learning';
+favoriteTechnology = 'Facial recognition';
+console.log(favoriteTechnology); 
+// Retorna Facial recognition
