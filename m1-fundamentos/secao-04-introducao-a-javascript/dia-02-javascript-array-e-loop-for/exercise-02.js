a5667f6c0a180cb055edff9c5cf411e01399fa40
pfonsecafv <pfonsecafv@gmail.com>
2022-10-27 12:08:45 -0300

    Exercício 02 - Array informações nos objetos

---
+++ m1-fundamentos/secao-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/exercise-02.js
@@ -0,0 +1,7 @@
+let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+let searchForFirstTask = tasksList[0];
+console.log(searchForFirstTask);
+
+let searchForLastTask = tasksList[tasksList.length - 1];
+console.log(searchForLastTask);
