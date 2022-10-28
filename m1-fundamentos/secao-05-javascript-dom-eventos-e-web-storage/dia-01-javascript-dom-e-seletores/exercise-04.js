9eda648a68b7bdc6c81920969f6b7c615104bfaf
pfonsecafv <pfonsecafv@gmail.com>
2022-10-28 19:37:11 -0300

    Exercício 04.1 - DOM Função mudar texto na tag <p></p>

---
+++ m1-fundamentos/secao-05-javascript-dom-eventos-e-web-storage/dia-01-javascript-dom-e-seletores/exercise-04.js
@@ -0,0 +1,5 @@
+function changeText() {
+  let paragraph = document.getElementsByTagName("p")[1];
+  paragraph.innerHTML = "Serei um programador profissional de qualidade e sucesso!"
+};
+changeText();
