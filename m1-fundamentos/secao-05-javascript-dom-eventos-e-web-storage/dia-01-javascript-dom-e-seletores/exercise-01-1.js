67f578e508353299e64bcffe284d4a5d1a8a2054
pfonsecafv <pfonsecafv@gmail.com>
2022-10-28 15:12:58 -0300

    Primeira alteração em m1-fundamentos-5.1

---
+++ m1-fundamentos/secao-05-javascript-dom-eventos-e-web-storage/dia-01-javascript-dom-e-seletores/exercise-01-1.js
@@ -0,0 +1,8 @@
+  // const paragraph = document.getElementById("paragraph");
+  // paragraph.style.color = "blue";
+
+const h2 = document.getElementById("page-title").innerText = "The Lord Of The Rings"
+
+const paragraph = document.getElementById("paragraph").innerText = "Cruzeiro time do meu coração!";
+
+const H4 = document.getElementById("subtitle").innerText = "Primeiro contato com JavaScript DOM";
