7f09a417a05462f3b5f2b3e0c654d54119273e0d
pfonsecafv <pfonsecafv@gmail.com>
2022-10-28 19:49:32 -0300

    Exercício 04.2 - DOM Função mudar cor amarelo > verde

--- m1-fundamentos/secao-05-javascript-dom-eventos-e-web-storage/dia-01-javascript-dom-e-seletores/exercise-04.js
+++ m1-fundamentos/secao-05-javascript-dom-eventos-e-web-storage/dia-01-javascript-dom-e-seletores/exercise-04.js
@@ -3,3 +3,9 @@ function changeText() {
   paragraph.innerHTML = "Serei um programador profissional de qualidade e sucesso!"
 };
 changeText();
+
+function changeColorSquareGreen() {
+  let yellowSquare = document.getElementsByClassName("main-content")[0];
+  yellowSquare.style.background = "rgb(76,164,109)";
+}
+changeColorSquareGreen();
