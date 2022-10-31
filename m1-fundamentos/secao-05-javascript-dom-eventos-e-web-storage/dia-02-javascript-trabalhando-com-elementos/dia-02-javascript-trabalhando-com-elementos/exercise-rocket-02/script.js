cda2f4d4b2f77bf61b4ad1f782461649c9f1c0c8
pfonsecafv <pfonsecafv@gmail.com>
2022-10-31 20:45:07 -0300

    Exercício 02.1 :rocket: - Criando elementos - Criar um filho para

--- m1-fundamentos/secao-05-javascript-dom-eventos-e-web-storage/dia-02-javascript-trabalhando-com-elementos/dia-02-javascript-trabalhando-com-elementos/exercise-rocket-02/script.js
+++ m1-fundamentos/secao-05-javascript-dom-eventos-e-web-storage/dia-02-javascript-trabalhando-com-elementos/dia-02-javascript-trabalhando-com-elementos/exercise-rocket-02/script.js
@@ -11,3 +11,17 @@ function exercise01_2() {
   console.log(irmaoElementoOndeVoceEsta);
 }
 exercise01_2();
+
+function exercise02_1() {
+  const elementoOndeVoceEsta = document.getElementById("elementoOndeVoceEsta");
+  console.log(elementoOndeVoceEsta);
+}
+exercise02_1();
+
+function exercise02_2() {
+  const filhoElementoOndeVoceEsta = document.createElement("section");
+  filhoElementoOndeVoceEsta.id = "filhoElementoOndeVoceEsta";
+  elementoOndeVoceEsta.appendChild(filhoElementoOndeVoceEsta);
+  console.log(filhoElementoOndeVoceEsta);
+}
+exercise02_2();
