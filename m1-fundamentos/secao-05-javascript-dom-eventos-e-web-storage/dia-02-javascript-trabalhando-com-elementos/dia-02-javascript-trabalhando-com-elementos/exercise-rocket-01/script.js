eef1cc1e240b581903df35f8f95606b69224eca7
pfonsecafv <pfonsecafv@gmail.com>
2022-10-31 19:34:43 -0300

    Exercício 01.2 :rocket: - Buscando elementos - Acesse pai a partir de elementoOndeVoceEsta e adicione uma color a ele

--- m1-fundamentos/secao-05-javascript-dom-eventos-e-web-storage/dia-02-javascript-trabalhando-com-elementos/dia-02-javascript-trabalhando-com-elementos/exercise-rocket-01/script.js
+++ m1-fundamentos/secao-05-javascript-dom-eventos-e-web-storage/dia-02-javascript-trabalhando-com-elementos/dia-02-javascript-trabalhando-com-elementos/exercise-rocket-01/script.js
@@ -1,5 +1,11 @@
 function exercise01() {
-  const elementoOndeVoceEsta = document.getElementById('elementoOndeVoceEsta');
+  const elementoOndeVoceEsta = document.getElementById("elementoOndeVoceEsta");
   console.log(elementoOndeVoceEsta);
-};
+}
 exercise01();
+
+function exercise02() {
+  let pai = elementoOndeVoceEsta.parentElement;
+  pai.style.color = "red";
+}
+exercise02();
