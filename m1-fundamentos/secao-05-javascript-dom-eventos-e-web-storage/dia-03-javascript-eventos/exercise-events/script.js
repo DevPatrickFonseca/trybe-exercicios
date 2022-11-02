4a6125871e9f369df32ce9af30587d5b66eadd34
pfonsecafv <pfonsecafv@gmail.com>
2022-11-02 19:06:20 -0300

    Exercício :rocket: 01 - Calendário Trybe - Parte 01

---
+++ m1-fundamentos/secao-05-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/exercise-events/script.js
@@ -0,0 +1,54 @@
+const createDaysOfTheWeek = () => {
+  const weekDays = [
+    "Domingo",
+    "Segunda",
+    "Terça",
+    "Quarta",
+    "Quinta",
+    "Sexta",
+    "Sábado",
+  ];
+  const weekDaysList = document.querySelector(".week-days");
+
+  for (let index = 0; index < weekDays.length; index += 1) {
+    const days = weekDays[index];
+    const dayListItem = document.createElement("li");
+    dayListItem.innerHTML = days;
+
+    weekDaysList.appendChild(dayListItem);
+  }
+};
+
+createDaysOfTheWeek();
+
+// Parte 01 - Crie um calendário dinamicamente
+
+let decemberDaysList = [
+  29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+  21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
+];
+
+function createDaysOfTheMonth() {
+  let getDaysList = document.querySelector("#days");
+
+  for (let i = 0; i < decemberDaysList.length; i += 1) {
+    let day = decemberDaysList[i];
+    let dayItem = document.createElement("li");
+    dayItem.innerHTML = day;
+
+    if (day === 24 || day === 31) {
+      dayItem.className = "day holiday";
+      getDaysList.appendChild(dayItem);
+    } else if (day === 4 || day === 11 || day === 18) {
+      dayItem.className = "day friday";
+      getDaysList.appendChild(dayItem);
+    } else if (day === 25) {
+      dayItem.className = "day holiday friday";
+      getDaysList.appendChild(dayItem);
+    } else {
+      dayItem.className = "day";
+      getDaysList.appendChild(dayItem);
+    }
+  }
+};
+createDaysOfTheMonth();
