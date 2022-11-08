3d712651693171189873177e9d9525b683c808a1
pfonsecafv <pfonsecafv@gmail.com>
2022-11-08 15:32:08 -0300

    Exercício 03 - Forms - Prevent default

---
+++ m1-fundamentos/secao-06-html-e-css-forms-flexbox-e-responsivo/dia-01-html-e-css-forms/exercise-03/script.js
@@ -0,0 +1,24 @@
+// 01 - Adicione um evento click no elemento a que possui o id href. Na função do evento, implemente o método .preventDefault() para prevenir o comportamento padrão do elemento. Ou seja, ao clicar no link, nada deve acontecer.
+
+const INPUT_TEXT = document.querySelector("#input-text");
+const INPUT_CHECKBOX = document.querySelector("#input-checkbox");
+const HREF_LINK = document.querySelector("#href");
+
+HREF_LINK.addEventListener("click", (event) => {
+  event.preventDefault();
+});
+
+// 02 - Adicione um evento click no elemento checkbox que possui o id input-checkbox. Na função do evento, implemente o método .preventDefault() para prevenir o comportamento padrão do elemento. Ou seja, ao clicar na caixinha, nada deve acontecer.
+
+INPUT_CHECKBOX.addEventListener("click", (event) => {
+  event.preventDefault();
+});
+
+// 03 - Adicione um evento keypress no elemento input type="text" que possui o id input-text. Na função do evento, implemente o método .preventDefault() de forma que somente o caractere a (letra ‘a’ minúscula) possa ser digitado na caixinha. (dica: para capturar a tecla digitada, utilize o event.key)
+
+INPUT_TEXT.addEventListener("keypress", (event) => {
+  const character = event.key;
+  if (character !== "a") {
+    event.preventDefault();
+  }
+});
