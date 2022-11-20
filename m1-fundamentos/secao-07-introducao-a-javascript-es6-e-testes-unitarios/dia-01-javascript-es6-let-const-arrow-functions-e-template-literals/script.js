d4177d76858bcff5315a89c6fa17ece323c35f91
pfonsecafv <pfonsecafv@gmail.com>
2022-11-20 19:44:22 -0300

    Exercício 01.2 - Concluído

--- m1-fundamentos/secao-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
+++ m1-fundamentos/secao-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
@@ -30,3 +30,37 @@ const customer = {
   age: 22,
   job: 'Teacher',
 };
+
+// Exercicio 01.2 - Adicionando novas Chaves
+
+const customer1 = {
+  firstName: 'Roberto',
+  age: 22,
+  job: 'Teacher',
+};
+
+const customer2 = {
+  firstName: 'Maria',
+  age: 23,
+  job: 'Medic',
+};
+
+// Continuação
+
+const customer1 = {
+  firstName: 'Roberto',
+  age: 22,
+  job: 'Teacher',
+};
+
+console.log(customer1.firstName);
+
+// Continuação
+
+const customer2 = {
+  firstName: 'Maria',
+  age: 23,
+  job: 'Medic',
+};
+
+console.log(customer2['firstName']);
