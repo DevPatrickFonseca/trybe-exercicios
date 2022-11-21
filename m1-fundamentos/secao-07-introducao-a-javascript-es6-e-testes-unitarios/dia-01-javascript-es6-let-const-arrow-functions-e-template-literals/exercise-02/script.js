b0bc9aef0a939235653edc7367392573256e9fbc
pfonsecafv <pfonsecafv@gmail.com>
2022-11-21 18:25:11 -0300

    Organizando/Atualizando - Diretórios e arquivos

---
+++ m1-fundamentos/secao-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6-let-const-arrow-functions-e-template-literals/exercise-02/script.js
@@ -0,0 +1,11 @@
+// Exercicio 02.1 
+
+const verifyIsNumber = (value1, value2) => {
+}
+const sum = () => {
+  // ...
+}
+window.onload = () => {
+  const button = document.getElementById('button');
+  button.addEventListener('click', sum);
+}
