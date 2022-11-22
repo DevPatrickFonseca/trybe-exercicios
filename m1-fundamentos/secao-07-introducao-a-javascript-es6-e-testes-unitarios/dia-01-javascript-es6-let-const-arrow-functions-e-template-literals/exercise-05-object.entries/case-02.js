b32d0f8d27b58a069dfc814fb57d4c01d8440963
pfonsecafv <pfonsecafv@gmail.com>
2022-11-21 22:22:13 -0300

    Exercise 05 - Object.entries

---
+++ m1-fundamentos/secao-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6-let-const-arrow-functions-e-template-literals/exercise-05-object.entries/case-02.js
@@ -0,0 +1,10 @@
+// Case 02
+
+const coutries = {
+  franca: 'Paris',
+  brasil: 'Brasília',
+  espanha: 'Madrid',
+  portugal: 'Lisboa',
+};
+const pairKeyValue = Object.entries(coutries);
+console.log(pairKeyValue);
---
+++ m1-fundamentos/secao-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6-let-const-arrow-functions-e-template-literals/exercise-05-object.entries/case-03.js
@@ -0,0 +1,16 @@
+// Case 03
+
+const coutries = {
+  franca: 'Paris',
+  brasil: 'Brasília',
+  espanha: 'Madrid',
+  portugal: 'Lisboa',
+};
+const pairKeyValue = Object.entries(coutries);
+console.log(pairKeyValue);
+
+for(index in pairKeyValue) {
+  console.log('--------');
+  console.log('País:', pairKeyValue[index][0]);
+  console.log('Capital:', pairKeyValue[index][1]);
+};
