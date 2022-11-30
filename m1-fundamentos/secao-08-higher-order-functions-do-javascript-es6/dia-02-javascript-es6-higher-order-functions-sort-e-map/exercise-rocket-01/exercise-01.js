7da3f05421d96a58209f7fee1c6d591688037711
pfonsecafv <pfonsecafv@gmail.com>
2022-11-30 18:20:35 -0300

    Exercise :rocket: 01.3 - nameAndAge

--- m1-fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-02-javascript-es6-higher-order-functions-sort-e-map/exercise-rocket-01/exercise-01.js
+++ m1-fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-02-javascript-es6-higher-order-functions-sort-e-map/exercise-rocket-01/exercise-01.js
@@ -96,3 +96,41 @@ const formatedAuthorNamesBirth = () => {
 }
 
 console.log(formatedAuthorNamesBirth());
+
+// 03 - Construa um array de objetos a partir do array de livros.
+
+const expectedResult = [
+  {
+    author: 'Isaac Asimov',
+    age: 31,
+  },
+  {
+    author: 'H. P. Lovecraft',
+    age: 38,
+  },
+  {
+    author: 'Stephen King',
+    age: 39,
+  },
+  {
+    author: 'George R. R. Martin',
+    age: 43,
+  },
+  {
+    author: 'Frank Herbert',
+    age: 45,
+  },
+  {
+    author: 'J. R. R. Tolkien',
+    age: 62,
+  },
+];
+
+const nameAndAge = () => {
+  return books.map((book) => ({author: book.author.name, age: book.releaseYear - book.author.birthYear}))
+  .sort((arrayObjects1, arrayObjects2) => arrayObjects1.age - arrayObjects2.age);
+  // 01 - Recebe o nome do autor e calcula a idade do autor
+  // 02 - Ordena os autores por idade
+}
+
+console.log(nameAndAge());
