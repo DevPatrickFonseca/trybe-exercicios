e283a73b66c9c065966222b6ff7a7d653cff9386
pfonsecafv <pfonsecafv@gmail.com>
2022-11-29 21:29:13 -0300

    Exercise :rocket: 01.5 - someBookWasReleaseOnThe80s

--- m1-fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-01-javascript-es6-introducao-a-higher-order-functions/exercise-rocket-01/exercise1.js
+++ m1-fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-01-javascript-es6-introducao-a-higher-order-functions/exercise-rocket-01/exercise1.js
@@ -121,4 +121,12 @@ console.log(everyoneWasBornOnSecXX());
 
 // 05 - Faça uma função que retorne true, se algum livro foi lançado na década de 80, e false, caso contrário.
 
+const expectedResult5 = true;
+
+const someBookWasReleaseOnThe80s = () => {
+  return books.some((book) => book.releaseYear >= 1980 && book.releaseYear >= 1989);
+};
+
+console.log(someBookWasReleaseOnThe80s());
+
 // 06 - Faça uma função que retorne true, caso nenhuma pessoa autora tenha nascido no mesmo ano, e false, caso contrário.
