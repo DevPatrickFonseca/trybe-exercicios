3f41e2574d73588078b8fbd19bee2abaf0f992fa
pfonsecafv <pfonsecafv@gmail.com>
2022-12-01 20:15:37 -0300

    Exercise :rocket: 01.2 - Organizando biblioteca

--- m1-fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-03-javascript-es6-higher-order-functions-filter-e-reduce/exercise-rocket-01/exercise-02.js
+++ m1-fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-03-javascript-es6-higher-order-functions-filter-e-reduce/exercise-rocket-01/exercise-02.js
@@ -1,4 +1,5 @@
-// Organizando biblioteca - 01
+// Organizando biblioteca 02
+
 
 const books = [
   {
@@ -63,51 +64,46 @@ const books = [
   },
 ];
 
-// 01 - Filtre todos os objetos do gênero ficção científica ou fantasia.
+
+// 02 - Filtre os livros com mais de 60 anos desde sua publicação e ordene a partir do livro mais velho para o mais novo.
 
 const expectedResult = [
   {
-    id: 1,
-    name: 'As Crônicas de Gelo e Fogo',
-    genre: 'Fantasia',
-    author: { name: 'George R. R. Martin', birthYear: 1948 },
-    releaseYear: 1991
-  },
-  {
-    id: 2,
-    name: 'O Senhor dos Anéis',
-    genre: 'Fantasia',
-    author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
-    releaseYear: 1954
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: { name: 'H. P. Lovecraft', birthYear: 1890 },
+    releaseYear: 1928,
   },
   {
     id: 3,
     name: 'Fundação',
     genre: 'Ficção Científica',
     author: { name: 'Isaac Asimov', birthYear: 1920 },
-    releaseYear: 1951
+    releaseYear: 1951,
   },
   {
-    id: 4,
-    name: 'Duna',
-    genre: 'Ficção Científica',
-    author: { name: 'Frank Herbert', birthYear: 1920 },
-    releaseYear: 1965
-  }
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
+    releaseYear: 1954,
+  },
 ];
 
-// Modo 01
+// anoAtual - anoPublicacao
 
-const fantasyOrScienceFiction = () => {
-  return books.filter((book) => {
-    return book.genre === 'Ficção Científica' || book.genre === 'Fantasia';
-  });
+// const currentYear = new Date().getFullYear();
+// console.log(currentYear);
+
+const oldBooksOrdered = () => {
+  const currentYear = new Date().getFullYear(); // Script que retorna ano atual
+
+  return books
+    .filter((book) => (currentYear - book.releaseYear) >= 60)
+      // Retorna apenas livros com mais de 60 anos
+    .sort((a, b) => a.releaseYear - b.releaseYear);
+      // Ordena livros por mais novo
 };
 
-console.log(fantasyOrScienceFiction());
-
-// Modo 02
-
-const fantasyOrScienceFiction1 = () => books.filter((book) => book.genre === 'Ficção Científica' || book.genre === 'Fantasia');
-
-console.log(fantasyOrScienceFiction1());
+console.log(oldBooksOrdered());
