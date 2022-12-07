f480df9f73c3f56b355b36839d5f15fb56fdf2b7
pfonsecafv <pfonsecafv@gmail.com>
2022-12-07 15:37:03 -0300

    Exercise 05 - personLikes

---
+++ m1-fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-04-javascript-es6-spread-operator-rest-parameters-e-object-destructuring/exercise-05.js
@@ -0,0 +1,26 @@
+// Exercício 3 - Escreva a função personLikes, que recebe como parâmetro os objetos alex ou gunnar. Cada objeto representa uma pessoa, e a função deve retornar todos os gostos daquela pessoa, conforme mostrado abaixo:
+
+// name: nome da pessoa, age: idade, likes: gosta de, nationality: nacionalidade
+
+const alex = {
+  name: 'Alex',
+  age: 26,
+  likes: ['fly fishing'],
+  nationality: 'Australian',
+};
+
+const gunnar = {
+  name: 'Gunnar',
+  age: 30,
+  likes: ['hiking', 'scuba diving', 'taking pictures'],
+  nationality: 'Icelandic',
+};
+
+// complete a assinatura da função abaixo
+const personLikes = ({name, age, likes}) => (
+  `${name} is ${age} years old and likes ${likes.join(', ')}.`
+);
+// <nome> tem <anos de idade> e gosta de <gostos da pessoa>
+
+console.log(personLikes(alex));
+console.log(personLikes(gunnar));
