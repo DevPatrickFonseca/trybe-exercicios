b6196a9a5b6bea157a8c3fb14a5bd8679a2cc7a5
pfonsecafv <pfonsecafv@gmail.com>
2022-12-07 18:28:52 -0300

    Exercise 02 - array-destructuring ariáveis incoerentes

---
+++ m1-fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-array-destructuring-default-destructuring-object-property-shorthand-e-default-parameters/exercise-02-array-destructuring.js
@@ -0,0 +1,11 @@
+// Exercício 02 - A seguir, temos alguns valores que estão descritos em variáveis incoerentes. Através da desestruturação de arrays, corrija os valores das variáveis.
+
+let comida = 'gato';
+let animal = 'água';
+let bebida = 'arroz';
+
+console.log(comida, animal, bebida); // arroz gato água
+
+[comida, animal, bebida] = [bebida, comida, animal];
+
+console.log(comida, animal, bebida);
