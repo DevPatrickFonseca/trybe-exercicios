7bea5b70127245c382aa909394484c4288126af2
pfonsecafv <pfonsecafv@gmail.com>
2022-12-07 15:43:21 -0300

    Exercise :rocket: 01 - filterPeople

---
+++ m1-fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-04-javascript-es6-spread-operator-rest-parameters-e-object-destructuring/exercise-rocket-01.js
@@ -0,0 +1,38 @@
+// 🚀 Exercício 4 - Escreva uma função filterPeople que, dada uma lista de pessoas, retorna todas as pessoas australianas que nasceram no século 20:
+
+const people = [
+  {
+    name: 'Nicole',
+    bornIn: 1992,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Harry',
+    bornIn: 2008,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Toby',
+    bornIn: 1901,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Frida',
+    bornIn: 1960,
+    nationality: 'Dannish',
+  },
+  {
+    name: 'Fernando',
+    bornIn: 2001,
+    nationality: 'Brazilian',
+  },
+  // bornIn: nascido em
+];
+
+const filterPeople = (arrayPeople) => {
+  return people.filter(({nationality, bornIn}) => {
+    return nationality === 'Australian' && bornIn >1980 & bornIn <= 2000;
+  })
+};
+
+console.log(filterPeople());
