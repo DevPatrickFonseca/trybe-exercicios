2d486ada3e2f0fd55ebc497b7a66b75985e42de0
pfonsecafv <pfonsecafv@gmail.com>
2022-12-07 15:49:59 -0300

    Exercise 07 - yearSeasons

---
+++ m1-fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-04-javascript-es6-spread-operator-rest-parameters-e-object-destructuring/exercise-07.js
@@ -0,0 +1,16 @@
+// Exercício 6 - O objeto yearSeasons representa as estações do ano. Cada chave do objeto é uma estação, e o valor de cada chave é um array de strings que representa os meses daquela estação. A partir desse objeto, desestruture as estações do ano e espalhe-as em um array de meses do ano.
+// Obs. 💬: As estações do ano aqui descritas têm base nas estações do hemisfério norte.
+
+const yearSeasons = {
+  spring: ['March', 'April', 'May'],
+  summer: ['June', 'July', 'August'],
+  autumn: ['September', 'October', 'November'],
+  winter: ['December', 'January', 'February'],
+};
+// yearSeasons: estações do ano.
+
+const { spring, summer, autumn, winter } = yearSeasons;
+
+const months = [...spring, ...summer, ...autumn, ...winter];
+
+console.log(months);
